8089631d18c91e57c2fc8e6be4c6983e94326dba
Frontent Developer <107357305+marufjon2001@users.noreply.github.com>
2023-09-11 17:36:33 +0500

    misterbyt

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+    screens: {
+      'xs': {'max': '320px'},
+      'ss': {'max': '375px'},
+      'sm': {'max': '768px'},
+      'md': {'max': '1024px'},
+      'lg': {'max': '1440px'},
+      'xl': {'max': '1700px'},
+    },
+  },
+  plugins: [],
+}
+
